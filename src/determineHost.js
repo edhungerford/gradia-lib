18f111377cbcc8d910663e8f58cb0d58dcda64e6
edhungerford <79060679+edhungerford@users.noreply.github.com>
2025-12-23 00:32:15 -0500

    Snaplands.

--- src/determineHost.js
+++ src/determineHost.js
@@ -5,23 +5,23 @@ host = window.location.host;
  determineTitle (){
     if(this.host == "bsbs.edsite.black"){
       return "Blue Skies, Black Smoke";
-    } else if (this.host == "shadowsong.edsite.black"){
-        return "Shadowsong";
+    } else if (this.host == "snaplands.edsite.black"){
+        return "South of Snaplands";
     } else if (this.host == "gradia.edsite.black"){
         return "Tails of Gradia";
-    }
-    return "Blue Skies, Black Smoke";
+    } 
+    return "Snaplands";
   }
 
    determineFetch (){
     if(this.host == "bsbs.edsite.black"){
       return "bsbs";
-    } else if (this.host == "shadowsong.edsite.black"){
-        return "shadowsong";
+    } else if (this.host == "snaplands.edsite.black"){
+        return "snaplands";
     } else if (this.host == "gradia.edsite.black"){
         return "gradia";
     }
-    return "bsbs";
+    return "snaplands";
   }
 
 }
