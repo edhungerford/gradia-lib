9773b351e74ca7235d2564e81ed2f2f015722699
edhungerford <79060679+edhungerford@users.noreply.github.com>
2026-02-01 14:40:30 -0500

    Shrinking scalesagas image. Just use firefox lol

--- src/determineHost.js
+++ src/determineHost.js
@@ -12,7 +12,7 @@ host = window.location.host;
     } else if (this.host === "scalesagas.edsite.black"){
       return "Scalesagas";
     }
-    return "South of Snaplands";
+    return "Scalesagas";
   }
 
    determineFetch (){
@@ -25,7 +25,7 @@ host = window.location.host;
     } else if (this.host === "scalesagas.edsite.black"){
       return "scalesagas";
     }
-    return "snaplands";
+    return "scalesagas";
   }
 
 }
